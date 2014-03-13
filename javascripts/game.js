bbc8b155bb04fb3bf92d996320c3ffec4ffc9f36
Matheus Pereira <matheper@gmail.com>
2014-03-13 00:41:57 -0300

    Added paddle draw and movement.

--- javascripts/game.js
+++ javascripts/game.js
@@ -2,6 +2,9 @@ var canvas;
 var context;
 var framerate = 20;
 var frameskip = framerate/5;
+var P1;
+var P2;
+var paddle_speed = 4;
 
 window.onload = function(){
     startUp();
@@ -19,10 +22,15 @@ function startUp(){
     window.setInterval(gameLoop, framerate);
     canvas = document.getElementById('game_canvas');
     context = canvas.getContext('2d');
+    P1 = Player(1);
+    P2 = Player(2);
 }
 
 function gameLoop(){
     InputManager.proccessInput();
+    clearCanvas();
+	drawPlayer(P1);
+	drawPlayer(P2);
 }
 
 //Input Management
@@ -46,12 +54,14 @@ var InputManager = {
     //players actions
     proccessInput: function(){
         //player1 action
-        var directionP1 = (this.isKeyPressed(38) ? -1 : 0) + //up
-                          (this.isKeyPressed(40) ?  1 : 0);  //down
+        var directionP1 = (this.isKeyPressed(87) ? -1 : 0) + //w
+                          (this.isKeyPressed(83) ?  1 : 0);  //s
+        P1.move(directionP1);
 
         //player2 action
-        var directionP2 = (this.isKeyPressed(87) ? -1 : 0) + //w
-                          (this.isKeyPressed(83) ?  1 : 0);  //s
+        var directionP2 = (this.isKeyPressed(38) ? -1 : 0) + //up
+                          (this.isKeyPressed(40) ?  1 : 0);  //down
+        P2.move(directionP2);
     }
 }
 
@@ -59,16 +69,30 @@ var InputManager = {
 Player = function(side){
     var x;
     var y;
+    var sprite = new Image();
+    sprite.src= "images/paddle.jpg";
     if(side==1){
         x = 10;
-        y = 10;
+        y = 160;
     }
     else{
-        x = 20;
-        y = 20;
+        x = 575;
+        y = 160;
     }
     return {
         x:x,
         y:y,
+        sprite:sprite,
+        move: function(my){
+            this.y += my*paddle_speed;
+        }
     }
+}
+
+function drawPlayer(player){
+    context.drawImage(player.sprite, player.x, player.y);
+}
+
+function clearCanvas(){
+    canvas.width = canvas.width;
 }
